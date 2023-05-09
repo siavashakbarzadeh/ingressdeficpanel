6e1fb9c281602bb9b02ead01c80ace7541356f92
mralikeshtkar <alikeshtkar262@gmail.com>
2023-05-09 20:17:49 +0330

    123

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        dd("ok");
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
 
